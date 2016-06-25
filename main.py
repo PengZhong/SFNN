48cd87d2dfacd95cf1397c0792c0a2d98db542e8
Hound <pengmany@outlook.com>
2016-06-25 15:39:13 +0800

    add assessment to main

--- main.py
+++ main.py
@@ -3,15 +3,17 @@
 
 """
 import training_test_data
+import assessment
 import os
 # import runpy
 
 # generate training file and test file
 source_data_path = r'.\data\iris.txt'
-training_file_path = r'.\data\training3.dat'
-testing_file_path = r'.\data\test3.dat'
+training_file_path = r'.\data\training4.dat'
+testing_file_path = r'.\data\test4.dat'
 training_test_data.generate_training_testing_data(source_data_path, training_file_path, testing_file_path)
-output_path = r'.\result\output3.csv'
+output_path = r'.\result\output4.csv'
 
 # runpy.run_path(r'.\sfnn_without_thin.py training_file_path testing_file_path output_path')
 os.system("python .\sfnn_without_thin.py %s %s %s" % (training_file_path, testing_file_path, output_path))
+print "accuracy:", assessment.accuracy(output_path)
