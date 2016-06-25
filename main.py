c677858126b2ba31374d5416adea5618557da30a
Hound <pengmany@outlook.com>
2016-06-25 14:33:53 +0800

    normal change

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ import os
 
 # generate training file and test file
 source_data_path = r'.\data\iris.txt'
-training_file_path = r'.\data\training2.dat'
-testing_file_path = r'.\data\test2.dat'
+training_file_path = r'.\data\training3.dat'
+testing_file_path = r'.\data\test3.dat'
 training_test_data.generate_training_testing_data(source_data_path, training_file_path, testing_file_path)
-output_path = r'.\result\output2.csv'
+output_path = r'.\result\output3.csv'
 
 # runpy.run_path(r'.\sfnn_without_thin.py training_file_path testing_file_path output_path')
 os.system("python .\sfnn_without_thin.py %s %s %s" % (training_file_path, testing_file_path, output_path))
